2414905102afeddd3bfe9fc4fa7338116179296d
Sarah Betack <msbetack@gmail.com>
2017-10-08 15:11:32 -0500

    fixes

--- script.js
+++ script.js
@@ -9,11 +9,12 @@
 
 
 
+
 ///////////////YOUR VARIABLES SHOULD BE ABOVE THIS LINE////////////////////
 
 //////////////////////DO NOT CHANGE THE CODE BELOW//////////////////////////
 document.addEventListener('DOMContentLoaded', function() {
 
-  alert("Hi my name is " + myName + ". I am " + myAge + " years old, and my favorite color is " + myFavoriteColor + ". My favorite thing is " + myFavoriteHobby + "!")
+  alert("Hi my name is " + myName + ". I am " + myAge + " years old, and my favorite color is " + myFavoriteColor + ". My favorite hobby is " + myFavoriteHobby + "!")
 
 })
